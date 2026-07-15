74de2ac9be462b6418955de125d37ea265a8947d
KT <felikzz98@gmail.com>
2026-07-15 09:40:59 +0700

    feat: initialize Home and UserList pages with TypeScript interfaces

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import UsersList from "./pages/UserList";
 import "./App.css";
 
 function App() {
   return (
     <Router>
       <Routes>
-        // TODO3: เพิ่ม Route ไป Home ตรงนี้
-        <Route path="/" element={} />
-        // TODO4: เพิ่ม Route ไป UsersList ตรงนี้
-        <Route path="/users" element={} />
+        {/* TODO3: เพิ่ม Route ไป Home ตรงนี้ */}
+        <Route path="/" element={<Home name="John" />} />
+        {/* TODO4: เพิ่ม Route ไป UsersList ตรงนี้ */}
+        <Route path="/users" element={<UsersList />} />
       </Routes>
     </Router>
   );
